c17b0211c85031d2b3411080b1686fe28efeab38
SahilKalra11 <sahilkalra2108@gmail.com>
2025-09-23 01:06:56 +0200

    add changes in about

--- en-us/about-us/index.php
+++ en-us/about-us/index.php
@@ -5,7 +5,7 @@
 <head>
 
     <meta charset="utf-8">
-    <title>About Xexec And Our Employee &amp; Customer Loyalty Solutions</title> 
+    <title>About Xexec And Our Employee &amp; Customer Loyalty Solutions</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Xexec is an award winning reward &amp; recognition, employee benefits and customer loyalty solutions provider in the UK trusted by more than 1 million people.">
@@ -32,17 +32,18 @@
         <div class="container">
           <div class="row">
                 <div class="col-lg-12 text-center">
-			
+
 				<h1 class="section-heading">About Us</h1>
                 <p>Xexec helps companies create a culture of engagement and motivation through social recognition and meaningful employee rewards.</p>
              </div>
             </div>
+            <div class="about-us"></div>
              </div>
-		
+
     </section>
 
-   
-    
+
+
     <!-- About Section -->
     <section id="aboutus">
         <div class="container">
@@ -50,9 +51,9 @@
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">Meaningful Achievements Leading to Meaningful Outcomes</h2>
                     <p>Xexec can help you build an employee recognition program centered around your company culture and core values. Our focus is on creating meaningful experiences for your employees that reflect your culture and values.</p>
-                    
-				
-					
+
+
+
 					<div class="col-lg-6">
 						<h3>We are an Engagement Company</h3>
 						<p>Helping you to motivate and engage your employees by creating a culture of recognition. By focusing on employee engagement we can ensure high rates of adoption and maximize your return on investment</p>
@@ -63,10 +64,10 @@
 						<!-- <h3>We are a customer services company</h3>
 						<p>Managing our own in-house Concierge Service including our highly acclaimed travel concierge, ticket concierge and lifestyle concierge with global availability and 24-hour customer service.</p> -->
                 </div>
-					
-					
-					
-					
+
+
+
+
 					<div class="col-lg-6">
 						<h3>We are Flexible and Client-Focused</h3>
 						<p>Big enough to take care of your global requirements; small enough to proactively manage our relationships with our customers and end-users.</p>
@@ -77,15 +78,15 @@
 						<!-- <h3>We are a product sourcing &amp fulfilment company</h3>
 						<p>Negotiating the very best discounts and offers for your employees and customers, with over 3,000 partnering vendor relationships, whilst managing local and international delivery.</p> -->
                 </div>
-					
-					
+
+
                 </div>
             </div>
-           
+
         </div>
     </section>
-    
-      
+
+
     <!-- About Section -->
     <section id="jointeam">
 	<div class="overlay">
@@ -100,10 +101,10 @@
            </div>
         </div>
     </section>
-    
+
     <section id="client">
         <div class="container">
-            <div class="col-lg-12 text-center">  
+            <div class="col-lg-12 text-center">
                 <h2 class="section-heading ">Happy Clients</h2>
 		    </div>
             <div class="row">
@@ -143,18 +144,18 @@
             </div>
         </div>
     </section>
-    
+
 
 
     <section id="testimonials">
         <div class="container">
-     
-                <div class="col-lg-12 text-center">  
+
+                <div class="col-lg-12 text-center">
                     <h2 class="section-heading ">Happy Customers</h2>
 			    </div>
                 <div class="form-group">&nbsp;</div>
                        <div class="row form-group">
-							
+
 								<div class="col-sm-3"><img src="../../programs/img/hilton.jpg" class="img-responsive center-block"/></div>
 								<div class="col-sm-3"><img src="../../programs/img/t-mobile.jpg" class="img-responsive center-block"></div>
 								<div class="col-sm-3"><img src="../../programs/img/hp-logo.jpg" class="img-responsive center-block"/></div>
@@ -163,41 +164,41 @@
                         <div class="form-group">&nbsp;</div>
 						   <div class="row"></div>
                              <div class="row form-group">
-                             
-								
+
+
 								<div class="col-sm-3"><img src="../../programs/img/ihop-logo.jpg" class="img-responsive center-block"/></div>
 								<div class="col-sm-3"><img src="../../programs/img/citibank.jpg" class="img-responsive center-block"/></div>
 								<div class="col-sm-3"><img src="../../programs/img/bank-of-america.jpg" class="img-responsive center-block"/></div>
 								<div class="col-sm-3"><img src="../../programs/img/applebees.jpg" class="img-responsive center-block"></div>
-                       
-								
+
+
                         </div>
-                        
+
                          <div class="row">
-                <div class="col-lg-12 text-center">   
-                    
-                    
+                <div class="col-lg-12 text-center">
+
+
                     <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  
+
     <div class="carousel-inner" role="listbox">
 
       <div class="item active">
-	
+
 
 <p><img src="../../programs/img/colt-logo.png"/></p>
 			<p>&ldquo;Choosing Xexec was fundamental to success. I see them as a true partner in what we did from
 advisory and support perspective. Without Xexec it wouldn’t have been the success it was.&rdquo;</p>
 			<p><strong>Masha Boldyreva, VP - HR Global Reward at Colt Technology Services</strong></p>
-			
+
       </div>
 
-    
-      
-		
-  
+
+
+
+
     </div>
 
-   <!-- <!-- Left and right controls 
+   <!-- <!-- Left and right controls
     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
@@ -208,54 +209,54 @@ advisory and support perspective. Without Xexec it wouldn’t have been the succ
     </a>-->
   </div>
 </div>
-                   
-                   
-                   
-                                      
+
+
+
+
                 </div>
-        
-           
+
+
         </div>
     </section>
-    
-    
-    
-    
+
+
+
+
     <section id="requestdemo">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">Get in Touch</h2>
-              
+
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-12">
                     <form name="sentMessage" id="contactForm" novalidate>
-                       
-                           
+
+
                          <div class="row">
-                            
+
                                <div class="col-md-6 form-group">
                                     <label for="firstname" class="control-label">First Name</label><p class="help-block text-danger"></p>
                                     <input type="text" class="form-control"  id="firstname" required data-validation-required-message="Enter your name.">
                             </div>
-                              
+
                                <div class="col-md-6 form-group">
                                     <label for="lastname" class="control-label">Last Name</label> <p class="help-block text-danger"></p>
                                     <input type="text" class="form-control"  id="lastname" required data-validation-required-message="Enter your last name.">
                             </div>
-                              
+
 						</div>
-                              
+
                         <div class="row">
-                             
-                             
+
+
                              <div class="col-md-4 form-group">
                                      <label for="company" class="control-label">Company Name</label> <p class="help-block text-danger"></p>
                                     <input type="text" class="form-control" id="company" required data-validation-required-message="Enter your company name.">
                             </div>
-                              
+
                               <div class="col-md-4 form-group">
                                      <label for="jobtitle" class="control-label">Job Title</label> <p class="help-block text-danger"></p>
                                     <input type="text" class="form-control" id="jobtitle" required data-validation-required-message="Enter your job title.">
@@ -273,24 +274,24 @@ advisory and support perspective. Without Xexec it wouldn’t have been the succ
 								</select>
                             </div>
                         </div>
-                               
+
                            <div class="row">
-                            
+
                                 <div class="col-md-6 form-group">
                                      <label for="phonenumber" class="control-label">Phone Number</label> <p class="help-block text-danger"></p>
                                     <input type="tel" class="form-control"  id="phonenumber" required data-validation-required-message="Enter your phone number.">
                             </div>
-                              
+
                                 <div class="col-md-6 form-group">
                                      <label for="email" class="control-label">Email</label> <p class="help-block text-danger"></p>
                                     <input type="email" class="form-control"  id="email" required data-validation-required-message="Enter your email address.">
                             </div>
-                              
-						</div>    
-                                  
-                            
-                            
-                            
+
+						</div>
+
+
+
+
                             <div class="clearfix"></div>
                             <div class="col-lg-12 text-center">
                                 <div id="success"></div>
@@ -306,4 +307,3 @@ advisory and support perspective. Without Xexec it wouldn’t have been the succ
          include("../includes/footer-above.php");
          include("../includes/footer-lower.php");
     ?>
-   
